794327a7cbd0a68c93acf0cb2f88a91c87713600
Josh Glover <jmglov@gmail.com>
2023-01-01 16:27:20 +0100

    Fix Hello Blambda example

--- examples/hello-world/src/hello.clj
+++ examples/hello-world/src/hello.clj
@@ -1,6 +1,6 @@
 (ns hello)
 
-(defn hello [{:keys [name] :or {name "Blambda"} :as event} context]
+(defn handler [{:keys [name] :or {name "Blambda"} :as event} context]
   (prn {:msg "Invoked with event",
         :data {:event event}})
   {:greeting (str "Hello " name "!")})
